a4a0498289b67b2f3a91ad1241ea8ca15d292adc
Dork09 <dor0908@gmail.com>
2026-01-01 14:02:58 +0200

    setup typescript runtime (index.ts + env)

---
+++ index.ts
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const postRoutes = require("./routes/postRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+app.use("/post", postRoutes);
+app.use("/comment", commentRoutes);
+
+const PORT = process.env.PORT;
+
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+    const db = mongoose.connection;
+   db.on("error", (error: Error) => console.error(error));
+   db.once("open", () => console.log("Connected to MongoDB"));
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (err: unknown) {
+  const message = err instanceof Error ? err.message : String(err);
+  console.error("Mongo connection failed:", message);
+  process.exit(1);
+}
+
+}
+
+startServer();
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,6 +6,7 @@ router.get("/", postController.getPost); //pass pointer to the function
 router.get("/:post_id", postController.getPostById); //pass pointer to the function
 
 router.post("/", postController.createPost);
+
 router.put("/:post_id", postController.updatePost);
 router.delete("/:post_id", postController.deletePost);
 
